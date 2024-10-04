36c2691406a3e26d559411ad9b5d48fc03f5a48f
Jun0613-spec <kumo5110@gmail.com>
2024-10-04 16:14:10 +0100

    Leetcode 1732.Find the Fighest Altitude java solution

---
+++ PrefixSum/FindTheHighestAltitude.java
@@ -0,0 +1,23 @@
+package PrefixSum;
+
+public class FindTheHighestAltitude {
+    public static void main(String[] args) {
+        System.out.println(highestAltitude(new int[]{-5, 1, 5, 0, -7})); // Output: 1
+        System.out.println(highestAltitude(new int[]{-4, -3, -2, -1, 4, 3, 2})); // Output: 0
+    }
+
+    public static int highestAltitude(int[] gain) {
+        int curr = 0;
+        int max = 0;
+
+        for(int i = 0; i < gain.length; i++) {
+            curr += gain[i];
+
+            if(curr > max) {
+                max = curr;
+            }
+        }
+
+        return max;
+    }
+}
